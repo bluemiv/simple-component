a05007ebdff9f16163c59d0e9931a464e62446ea
taehongkim <public.bluemiv@gmail.com>
2022-01-06 15:27:24 +0900

    Add react component & webpack config

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SimpleComponent from './components/SimpleComponent';
+
+ReactDOM.render(<SimpleComponent />, document.getElementById('root'));
